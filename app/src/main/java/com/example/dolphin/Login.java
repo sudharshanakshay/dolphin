95c6f2752403aab64295dbcd72d9ab262fbc338e
sudharshan <sudharshan6acharya@gmail.com>
2021-08-05 08:36:09 +0530

    added redirect to login & register in register & login page respectively

--- app/src/main/java/com/example/dolphin/Login.java
+++ app/src/main/java/com/example/dolphin/Login.java
@@ -1,19 +1,19 @@
 package com.example.dolphin;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
 
-    Button signIn_button;
+    Button signIn_button, redirect_to_register;
     EditText username,password ;
+    Context _context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +23,7 @@ public class Login extends AppCompatActivity {
         signIn_button = (Button) findViewById(R.id.signIn);
         username = (EditText) findViewById(R.id.l_username);
         password = (EditText) findViewById(R.id.l_password);
+        redirect_to_register = (Button) findViewById(R.id.b_redirectToSignUp);
 
 
         signIn_button.setOnClickListener(new View.OnClickListener() {
@@ -33,6 +34,16 @@ public class Login extends AppCompatActivity {
             }
 
 
+        });
+
+        redirect_to_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, Register.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
         });
     }
 }
